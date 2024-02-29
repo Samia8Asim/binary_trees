a6afe2c699ea0892c522c471d888dc4c085152a0
Samia8Asim <samia2003.8@gmail.com>
2024-02-29 22:03:24 +0200

    3

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,16 +8,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t dep = 0;
-
-	if (!tree)
-		return (0);
-
-	while (!tree->parent)
-	{
-		dep++;
-		tree = tree->parent;
-	}
-
-	return (dep);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
